19e54896a5fd22deefe56f2fbd9c7eacbf5ccfab
luvres <luvres@hotmail.com>
2023-08-22 22:36:17 -0300

    Downloads weights

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Fine-Tuning LLM: QLoRA, bitsandbytes
+Instruct-Tuning and Fine-Tuning LLM: QLoRA, bitsandbytes
 -----
-### Apptainer Build
+### Build container image
 ```
 apptainer build llm.sif llm.def
 ```
@@ -11,7 +11,8 @@ sudo mv llm.sif /scratch/images/
 
 ### Run (2.0.1+cu118 | True | Python 3.10.12 | CUDA 11.8.0)
 ```
-export IMAGE="/scratch/images/llm.sif"  
+export APPTAINER_BIND="/scratch/LLM/BLOOM/bloomz-3b/"
+export IMAGE="/scratch/images/llm.sif"
 
 apptainer run --nv ${IMAGE} python --version
 
@@ -21,3 +22,32 @@ apptainer run --nv ${IMAGE} python -c "import torch;print(torch.cuda.is_availabl
 
 apptainer shell --nv ${IMAGE}
 ```
+
+# Download Bloomz
+
+### bigscience/bloomz-3b (3B)
+```
+git lfs install
+git clone https://huggingface.co/bigscience/bloomz-3b
+```
+### bigscience/bloomz-7b1 (7.1B)
+```
+git lfs install
+git clone https://huggingface.co/bigscience/bloomz-7b1
+```
+### bigscience/bloomz (176B)
+```
+git lfs install
+git clone https://huggingface.co/bigscience/bloomz
+```
+
+# Download [OpenLLaMA](https://github.com/openlm-research/open_llama)
+
+### openlm-research/open_llama_7b_v2
+```
+git lfs install
+git clone https://huggingface.co/openlm-research/open_llama_7b_v2
+```
+
+
+
--- script.py
+++ script.py
@@ -1,12 +1,171 @@
-import argparse
-import bitsandbytes as bnb
-from datasets import load_dataset
-from functools import partial
-import os
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+###############################
+# 1) Load and Tokenizer Model #
+###############################
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed, Trainer, TrainingArguments, BitsAndBytesConfig, \
-    DataCollatorForLanguageModeling, Trainer, TrainingArguments
+print(torch.cuda.is_available())
+
+
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+pretrained_model = "/scratch/LLM/BLOOM/bloomz-3b"
+
+model = AutoModelForCausalLM.from_pretrained(
+    pretrained_model,
+    torch_dtype=torch.float16,
+    load_in_8bit=True,
+    device_map='auto',
+)
+
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+
+print(model)
+
+
+###################################
+# 2) Post-processing on the model #
+###################################
+for param in model.parameters():
+  param.requires_grad = False  # freeze the model - train adapters later
+  if param.ndim == 1:
+    # cast the small parameters (e.g. layernorm) to fp32 for stability
+    param.data = param.data.to(torch.float32)
+
+model.gradient_checkpointing_enable()  # reduce number of stored activations
+model.enable_input_require_grads()
+
+class CastOutputToFloat(nn.Sequential):
+  def forward(self, x): return super().forward(x).to(torch.float32)
+model.lm_head = CastOutputToFloat(model.lm_head)
+
+#################
+# 3) Apply LoRA #
+#################
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+### Initializing LoRA Config
+from peft import LoraConfig, get_peft_model
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["query_key_value"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+####################
+# 4) Preprocessing #
+####################
+import transformers
 from datasets import load_dataset
 
+dataset_name = "FourthBrainGenAI/MarketMail-AI"
+product_name = "product"
+product_desc = "description"
+product_ad = "marketing_email"
+
+dataset = load_dataset(dataset_name)
+print(dataset)
+print(dataset['train'][0])
+
+
+# We want to put our data in the form:
+# -----------
+### INSTRUCTION
+Below is a product and description, please write a marketing email for this product.
+
+### Product
+PRODUCT NAME
+
+### Description:
+DESCRIPTION
+
+### Marketing Email:
+OUR EMAIL HERE
+# -----------
+
+# Say you were fine-tuning a Natural Language to SQL task.
+# In that case - the data should be moved into the following format:
+# -----------
+### INSTRUCTION
+Please convert the following context into an SQL query.
+
+### CONTEXT
+How many people work at my company?
+
+### SQL
+SELECT COUNT(*) FROM employees
+# -----------
+
+
+### Because we're using BLOOMZ (which is an instruct-tuned base model), we should see better results providing the instruction - though that is not necessary.
+def generate_prompt(product: str, description: str, marketing_email: str) -> str:
+  prompt = f"### INSTRUCTION\nBelow is a product and description, please write a marketing email for this product.\n\n### Product:\n{product}\n### Description:\n{description}\n\n### Marketing Email:\n{marketing_email}"
+  return prompt
+
+mapped_dataset = dataset.map(lambda samples: tokenizer(generate_prompt(samples['product'], samples['description'], samples['marketing_email'])))
+
+
+### Share adapters on the 🤗 Hub
+HUGGING_FACE_USER_NAME = "YOUR USERNAME HERE"
+
+from huggingface_hub import notebook_login
+notebook_login()
+
+### If you run into issues during upload - please ensure you're using a HF Token with WRITE access!
+model_name = "YOUR MODEL NAME HERE"
+
+model.push_to_hub(f"{HUGGING_FACE_USER_NAME}/{model_name}", use_auth_token=True)
+
+
+### Load adapters from the Hub
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+peft_model_id = f"{HUGGING_FACE_USER_NAME}/{model_name}"
+config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=False)
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
+
+
+### Inference
+from IPython.display import display, Markdown
+
+def make_inference(product, description):
+  batch = tokenizer(f"### INSTRUCTION\nBelow is a product and description, please write a marketing email for this product.\n\n### Product:\n{product}\n### Description:\n{description}\n\n### Marketing Email:\n", return_tensors='pt')
+
+  with torch.cuda.amp.autocast():
+    output_tokens = model.generate(**batch, max_new_tokens=200)
+
+  display(Markdown((tokenizer.decode(output_tokens[0], skip_special_tokens=True))))
+
+your_product_name_here = "The Coolinator"
+your_product_description_here = "A personal cooling device to keep you from getting overheated on a hot summer's day!"
+
+make_inference(your_product_name_here, your_product_description_here)
 
