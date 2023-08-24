1a11cf6892f6e0d60837a3fb1669f175da6cfdfb
luvres <luvres@hotmail.com>
2023-08-24 14:05:02 -0300

    Create script for sabe adapters

--- adapter.py
+++ adapter.py
@@ -30,59 +30,59 @@ print(f'{model_subname}')
 ##    print('Missing input image')
 ##    returnpy
 
-model_id = "/scratch/LLM/BLOOM/{model_name}"
+model_id = f"/scratch/LLM/BLOOM/{model_name}"
 model_pretrained =  f"/scratch/{USER}/adapters/{model_name}-{model_subname}"
 
 print(f'{model_id}')
 print(f'{model_pretrained}')
 
-#bnb_config = BitsAndBytesConfig(
-#    load_in_4bit=True,
-#    bnb_4bit_use_double_quant=True,
-#    bnb_4bit_quant_type="nf4",
-#    bnb_4bit_compute_dtype=torch.bfloat16
-#)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
-#model = AutoModelForCausalLM.from_pretrained(
-#    model_id,
-#    quantization_config=bnb_config,
-#    device_map="auto"
-#)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    quantization_config=bnb_config,
+    device_map="auto"
+)
 
-#config = LoraConfig(
-#    r=16,
-#    lora_alpha=32,
-#    target_modules=["query_key_value"],
-#    lora_dropout=0.05,
-#    bias="none",
-#    task_type="CAUSAL_LM"
-#)
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["query_key_value"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
 
-##tokenizer = AutoTokenizer.from_pretrained(model_id)
-#model.gradient_checkpointing_enable()
-#model = prepare_model_for_kbit_training(model)
-#model = get_peft_model(model, config)
-#model.save_pretrained(model_pretrained)
-
-
-#print(model)
+#tokenizer = AutoTokenizer.from_pretrained(model_id)
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, config)
+model.save_pretrained(model_pretrained)
 
 
-#def print_trainable_parameters(model):
-#    """
-#    Prints the number of trainable parameters in the model.
-#    """
-#    trainable_params = 0
-#    all_param = 0
-#    for _, param in model.named_parameters():
-#        all_param += param.numel()
-#        if param.requires_grad:
-#            trainable_params += param.numel()
-#    print(
-#        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
-#    )
+print(model)
 
-#print_trainable_parameters(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+print_trainable_parameters(model)
 
 
 
