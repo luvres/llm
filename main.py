e67efd8fad167f68d4683ef04e7407fbd092ffea
luvres <luvres@hotmail.com>
2023-08-26 15:03:56 -0300

    Ajust parameters

--- main.py
+++ main.py
@@ -93,8 +93,10 @@ config = LoraConfig(
     task_type=lora_task_type
 )
 
+# Unsupervised fine-tuning
 if tuning == 'adapter':
     tokenizer = AutoTokenizer.from_pretrained(model_id)
+# Supervised fine-tuning
 elif peft_method == 'instruction':
     tokenizer = LlamaTokenizer.from_pretrained(model_id)
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
